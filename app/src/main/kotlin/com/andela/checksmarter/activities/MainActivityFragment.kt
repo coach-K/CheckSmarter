d680592b193c3a5329993c521b1ace3f6a26fd11
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-04-15 12:18:49 +0100

    Convert project to Kotlin

---
+++ app/src/main/kotlin/com/andela/checksmarter/activities/MainActivityFragment.kt
@@ -0,0 +1,24 @@
+package com.andela.checksmarter.activities
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.andela.checksmarter.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        hello.text = "Hello From Kotlin"
+
+    }
+}
---
+++ app/src/main/kotlin/com/andela/checksmarter/activities/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.andela.checksmarter.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by CodeKenn on 14/04/16.
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        //startActivity(intent)
+
+        //finish()
+    }
+}
