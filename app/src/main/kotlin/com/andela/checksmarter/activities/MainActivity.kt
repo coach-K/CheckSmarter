5147d284891b36b9de585b161bd1dcc7ff9ff31a
Kehinde Ogunde <kehinde.ogunde@andela.com>
2016-04-27 09:21:58 +0100

    Setup project for Kotlin

--- app/src/main/kotlin/com/andela/checksmarter/activities/MainActivity.kt
+++ app/src/main/kotlin/com/andela/checksmarter/activities/MainActivity.kt
@@ -3,9 +3,12 @@ package com.andela.checksmarter.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.andela.checksmarter.R
+import com.andela.checksmarter.fragments.MainActivityFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar)
+
+        newCheckSmarter.setOnClickListener {
+            getFragment().createNewCheckSmarter()
+            Log.d("HELLO", "CREATE}")
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -24,10 +32,14 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
 
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_speak) {
             return true
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    fun getFragment(): MainActivityFragment {
+        return supportFragmentManager.fragments[0] as MainActivityFragment
     }
 }
--- app/src/main/kotlin/com/andela/checksmarter/fragments/MainActivityFragment.kt
+++ app/src/main/kotlin/com/andela/checksmarter/fragments/MainActivityFragment.kt
@@ -1,24 +1,33 @@
 package com.andela.checksmarter.fragments
 
-import android.support.v4.app.Fragment
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.andela.checksmarter.R
-import kotlinx.android.synthetic.main.fragment_main.*
+import com.andela.checksmarter.views.CheckSmarterView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.properties.Delegates
 
 class MainActivityFragment : Fragment() {
+
+    var checkSmarterFragmentInnerView: CheckSmarterView by Delegates.notNull()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_main, container, false)
     }
 
-    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        hello.text = "Hello From Kotlin"
+        checkSmarterFragmentInnerView = view.findViewById(R.id.checkSmarterFragmentInnerView) as CheckSmarterView
+    }
 
+    fun createNewCheckSmarter() {
+        checkSmarterFragmentInnerView.createNewCheckSmarter()
+        Log.d("HELLO", "CREATE}")
     }
 }
